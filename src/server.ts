b65190bd3e0b7191161ba2a981e8bfea98384c99
naffiziqbal <f.nafiziqbal@gmail.com>
2024-04-07 20:49:42 +0600

    server/API root endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app/app";
 import mongoose from 'mongoose';
 import { config } from "./config";
+import { Request, Response } from "express";
 
 main().catch(err => console.log(err));
 
@@ -8,6 +9,10 @@ const port = config.port
 async function main() {
     try {
         await mongoose.connect(config.db_url as string);
+        app.get('/', (req: Request, res: Response) => {
+            res.status(200).json({ message: "Finance Management" })
+        })
+
         app.listen(port, () => `Server Is Listening on ${port}`)
         console.log(`Server Is Listening on ${port}`)
 
